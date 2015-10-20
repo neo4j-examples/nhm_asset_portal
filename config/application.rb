ef35dba79953ddffd3beebc20ecf3637dff31471
Brian Underwood <ml+github@semi-sentient.com>
2015-10-20 13:50:44 +0800

    Don't load ActiveRecord and move configuration to development.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+require 'neo4j/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,7 +28,5 @@ module NhmAssetPortal
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.neo4j.session_type = :server_db
-    config.neo4j.session_path = 'http://localhost:9888'
   end
 end
