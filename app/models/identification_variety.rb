a5b1e7937ee884039bcc85d5279c5fad494b2c05
Brian Underwood <ml+github@semi-sentient.com>
2015-10-21 16:07:30 +0800

    A couple more icons for models

--- app/models/identification_variety.rb
+++ app/models/identification_variety.rb
@@ -1,3 +1,7 @@
 class IdentificationVariety < GraphStarter::Asset
   has_many :in, :specimens, origin: :identification_variety
+
+  def self.icon_class
+    'unhide'
+  end
 end
--- app/models/state_province.rb
+++ app/models/state_province.rb
@@ -2,5 +2,9 @@ class StateProvince < GraphStarter::Asset
   has_one :out, :country, type: :IN_COUNTRY
   has_many :in, :localities, origin: :state_province
   has_many :in, :specimens, origin: :state_province
+
+  def self.icon_class
+    'flag'
+  end
 end
 
